f93b01614b36c5f55fe5f7a3414ed8700176f8f7
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-06 09:02:20 -0700

    Added middleware function

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// Retrieving request's IP Address Middleware------------------------
+// install: npm install request-ip --save
+// repo: https://github.com/pbojinov/request-ip
+const requestIp = require("request-ip");
+
+const ipMiddleware = function(req, res, next) {
+    const clientIp = requestIp.getClientIp(req);
+    next();
+};
+
+app.use(requestIp.mw());
+app.use(function(req, res) {
+    const ip = req.clientIp;
+    res.end(ip);
+});
+// ------------------------------------------------------------------
+
+
 
 // Port listening ---------------------------------------------------
 app.listen(PORT, () => {
